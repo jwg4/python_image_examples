b7c6a8afadcd406bbf0abde6aeb2816d291891d8
Jack Grahl <jack.grahl@gmail.com>
2019-12-17 23:17:31 +0000

    Implement filename correctly.

--- image_examples/helpers/image.py
+++ image_examples/helpers/image.py
@@ -1,7 +1,16 @@
+import os
+
+from pkg_resources import resource_filename
+
+
 class LazyImage():
     def __init__(self, relname):
         self.relname = relname
 
     @property
+    def _internal_name(self):
+        return os.path.join('data', self.relname)
+
+    @property
     def filename(self):
-        return self.relname
+        return resource_filename('image_examples', self._internal_name)
--- tests/test_alias.py
+++ tests/test_alias.py
@@ -1,3 +1,5 @@
+import os
+
 from unittest import TestCase
 
 from image_examples import aliases, peppers, phantom
@@ -7,7 +9,12 @@ class TestAliases(TestCase):
     def test_one_image_by_name(self):
         value = peppers.filename
         self.assertIsNotNone(value)
-        
+
+    def test_file_exists(self):
+        value = peppers.filename
+        exists = os.path.isfile(value)
+        self.assertTrue(exists, "%s does not exist" % (value, ))
+
     def test_another_image_by_name(self):
         value = phantom.filename
         self.assertIsNotNone(value)
