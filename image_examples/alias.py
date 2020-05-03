b35506e10712261ffd72a87f286c1a8165566ded
Jack Grahl <jack.grahl@gmail.com>
2020-05-03 12:58:07 +0000

    Add more images to alias.

--- image_examples/alias.py
+++ image_examples/alias.py
@@ -6,6 +6,9 @@ from .helpers.image import LazyImage
 
 cameraman = LazyImage("hlevkin/cameraman.bmp", "cameraman")
 fishing_boat = LazyImage("usc_misc/boat.512.tiff", "fishing_boat")
+gray = LazyImage("usc_misc/gray21.512.tiff", "gray")
+house = LazyImage("usc_misc/house.tiff", "house")
+ruler = LazyImage("usc_misc/ruler.512.tiff", "ruler")
 mandrill = LazyImage("usc_misc/4.2.03.tiff", "mandrill")
 peppers = LazyImage("usc_misc/4.2.07.tiff", "peppers")
 phantom = LazyImage("Shepp_logan.png", "phantom")
